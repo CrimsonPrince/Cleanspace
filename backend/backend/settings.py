65f3b09e7156ebd0d5f2872a8b8018a3c88d083d
crimsonprince <kingr4@protonmail.ch>
2019-11-25 17:52:29 +0000

    Added Temporary Society Serializer
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,9 +80,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+         'NAME': 'gis',
+         'USER':   r4,
+         'PASSWORD': test,
+         'HOST': 'r4.ie',
+         'PORT': '5434',
+    },
 }
 
 
