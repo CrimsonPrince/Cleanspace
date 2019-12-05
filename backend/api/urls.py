3d6064e32bf82aeac8a6c9a8d7e732b1260bd6e9
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-12-05 17:27:27 +0000

    Added Production Docker-Compose + Swagger
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.SocietyListView.as_view()),
+        path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
