5b0a1d24d8aa7503536419c22ac773e368b9602e
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-12-06 00:56:50 +0000

    Testing new CI Workflow
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,8 +5,4 @@ from . import views
 
 urlpatterns = [
     path('', views.SocietyListView.as_view()),
-        path('swagger-ui/', TemplateView.as_view(
-        template_name='swagger-ui.html',
-        extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui'),
 ]
