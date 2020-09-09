8954305b6f368bad3bddf18ccfe8d4feebc9df51
CrimsonPrince <CrimsonPrince@protonmail.com>
2020-09-09 12:16:25 +0100

    Another Commit
    Signed-off-by: r4 <kingr4@protonmail.ch>

--- src/routers/cities.js
+++ src/routers/cities.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Cities = require('../models/City')
 const pino = require('pino')
+const Measurements = require('../models/Measurements')
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' })
 
 const router = express.Router()
@@ -17,7 +18,16 @@ router.get('/cities/', async (req, res) => {
 
 router.get('/cities/:cityName', async (req, res) => {
     try {
-        city = await Cities.findCityByName(req.params.cityName)
+        city = await Measurements.getPast5Days(req.params.cityName)
+        res.send(city)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/cities/:cityName/:type', async (req, res) => {
+    try {
+        city = await Measurements.getPast5DaysType(req.params.cityName, req.params.type)
         res.send(city)
     } catch (error) {
         res.status(400).send(error)
